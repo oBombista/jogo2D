91eca43aa9ddb8c54b710e7265a716e96436b60f
Tiago Castro <59033674+oBombista@users.noreply.github.com>
2024-05-10 08:53:43 -0300

    Add files via upload

---
+++ jogo2D/shot.py
@@ -0,0 +1,24 @@
+import pygame
+import math
+import random
+
+class Shot(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = pygame.image.load("data/GorilaRed.png")
+        self.image = pygame.transform.scale(self.image, [10, 10])
+        self.rect = self.image.get_rect()
+
+        #Velocidade do Tiro
+        self.speed = 4
+
+    def update(self, *args, **kwargs):
+        #LOGICA dos Tiros
+        self.rect.x += self.speed
+
+        if self.rect.left > 840:
+            self.kill()
+
+
+
